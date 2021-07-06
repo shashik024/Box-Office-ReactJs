8cc0c1e9a4ccc5325ff782e8bdd32bf2dddb8265
shashi <shashik024@gmail.com>
2021-07-06 16:32:52 +0530

    Added layout managment and created title

--- src/Components/MainPageComponent.js
+++ src/Components/MainPageComponent.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Header from './Header';
+import Title from './Title';
 
 const MainPageComponent = ({ children }) => (
   <div>
+    <Title />
     <Header />
     {children}
   </div>
---
+++ src/Components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/*eslint-disable */
+const Title = () => {
+  return (
+    <div>
+      <h1>Box-Office</h1>
+      <p>Are you looking for a Movie or an Actor</p>
+    </div>
+  );
+};
+
+export default Title;
