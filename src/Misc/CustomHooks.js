3df1aabb49b08af41286ae6491402c24ac18bfa2
chahalrohit <shashik024@gmail.com>
2021-07-11 20:02:37 +0530

    custom hook created for show

---
+++ src/Misc/CustomHooks.js
@@ -0,0 +1,34 @@
+import { useReducer, useEffect } from 'react';
+
+// All hooks are functions
+// useReducer has third argument named initializer function  , it return the initial state
+
+function showReducer(prevState, action) {
+  switch (action.type) {
+    case 'ADD': {
+      return [...prevState, action.showId];
+    }
+    case 'REMOVE': {
+      return prevState.filter(showId => showId !== action.showId);
+    }
+
+    default:
+      return prevState;
+  }
+}
+
+function usePersistedReducers(reducer, initialState, key) {
+  const [state, dispatch] = useReducer(reducer, initialState, initial => {
+    const persisted = localStorage.getItem(key);
+    return persisted ? JSON.parse(persisted) : initial;
+  });
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
+
+  return [state, dispatch];
+}
+
+export function useShows(key = 'shows') {
+  return usePersistedReducers(showReducer, [], key);
+}
